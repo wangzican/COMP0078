508af8d227f2524a60dc5d4c9e049b1447ee13ea
Wang Zican Robert <zcabanj@ucl.ac.uk>
2022-11-30 17:36:02 +0000

    p1

--- CW2/kernel_perceptron.py
+++ CW2/kernel_perceptron.py
@@ -24,7 +24,6 @@ def train_test_split(dataset, ratio, split_label = True):
     
     train = np.array(train)
     test = np.array(test)
-
     # sometimes dont want to split the data and label
     if not split_label:
         return train, test
@@ -71,7 +70,7 @@ def n_fold(dataset, n):
 def visualize(digits):
     """
     Show the digit and its corresponding figure
-    digits: array of digits
+    digits: array of digits, with label
     """
     size = (16, 16)
     number = digits.shape[0]
@@ -88,14 +87,26 @@ def visualize(digits):
 def poly_kernel(xi, xj, degree):
     return xi.dot(xj.T) ** degree
 
+def gaussian_kernel(xi, xj, c):
+    """
+    Calculate the gaussian kernel matrix
+    """
+    matrix = np.zeros((xi.shape[0], xi.shape[0]))
+    for i in range(xi.shape[0]):
+        for j in range(xj.shape[0]):
+            matrix[i,j] = np.exp(-c * np.linalg.norm(xi[i]-xj[j], 1)**2)
+
+    return matrix
+
 class onevrest():
-    def __init__(self, X, Y, degree, number_of_class=10) -> None:
+    def __init__(self, X, Y, degree, number_of_class=10, kernel_function=poly_kernel) -> None:
         self.number_of_features = X.shape[1]
         self.number_of_data = X.shape[0]
         self.X = X
         self.Y = Y
         self.degree = degree
         self.number_of_class = number_of_class
+        self.kernel_function = kernel_function
         # weights for each 1vrest classifier, initialize to zeros
         self.weights = np.zeros((number_of_class, self.number_of_data))
 
@@ -104,7 +115,7 @@ class onevrest():
         Turning a 2 class calssifier into a multiclass one with a polynomial kernel
         """
         # kernel matrix, each (K(xi, xt)) would be each column of the matrix 
-        kernel = poly_kernel(self.X, self.X, self.degree)
+        kernel = self.kernel_function(self.X, self.X, self.degree)
 
         # getting "online" data in random order
         indices = np.arange(self.number_of_data)
@@ -131,7 +142,7 @@ class onevrest():
                     self.weights[y_hat, i] -= learning_rate
 
             mean_errors = errors/self.number_of_data
-            print("epoch ", epoch, ", errors: ", errors)
+            #print("epoch ", epoch, ", errors: ", errors)
             # break out if converges
             if epoch >= 2 and np.abs(previous_mean_erros - mean_errors) < threshold:
                 break
@@ -142,36 +153,46 @@ class onevrest():
         """
         predicting with the fitted weights
         """
-        kernel = poly_kernel(self.X, x_test, self.degree)
+        kernel = self.kernel_function(self.X, x_test, self.degree)
         y_hat = np.argmax(self.weights.dot(kernel))
         return y_hat
 
-    def test(self, x_test, y_test, detail=False):
+    def test(self, x_test, y_test, detail=False, count_each=False):
         """
         get the mean error for the whole test set
         """
+        error = 0
+        number_of_tests = x_test.shape[0]
         # placeholders
         confusion_mtr = np.zeros((self.number_of_class, self.number_of_class))
         number_of_sample_for_each_class = np.zeros(self.number_of_class)
+        errors_for_each = np.zeros(number_of_tests)
 
-        error = 0
-        number_of_tests = x_test.shape[0]
         for i in range(number_of_tests):
             kernel = poly_kernel(self.X, x_test[i], self.degree)
             y_hat = np.argmax(self.weights.dot(kernel))
             # counting the number of samples of each class
-            number_of_sample_for_each_class[int(y_test[i])] += 1
+            if detail:
+                number_of_sample_for_each_class[int(y_test[i])] += 1
             if y_hat != y_test[i]:
                 error += 1
                 # counting for the confusion matrix
-                confusion_mtr[int(y_test[i]), y_hat] += 1
+                if detail:
+                    confusion_mtr[int(y_test[i]), y_hat] += 1
+                if count_each:
+                    errors_for_each[i] += 1
+
 
         mean_error = error/number_of_tests
+        #print(number_of_sample_for_each_class)
         # normalize the mistakes in confusion matrix
-        for i in range(self.number_of_class):
-            confusion_mtr[i] = confusion_mtr[i]/number_of_sample_for_each_class[i]
+        
         if detail:
+            for i in range(self.number_of_class):
+                confusion_mtr[i] = confusion_mtr[i]/number_of_sample_for_each_class[i]
             return mean_error, confusion_mtr
+        if count_each:
+            return mean_error, errors_for_each
         return mean_error
 
 class onevone():
@@ -313,16 +334,16 @@ def P1Q2(digit_data):
     """
     Question 2
     """
-    train, test = train_test_split(digit_data, 0.8, split_label=False)
-    sets = n_fold(train, 2)
-
     # initialize list of d and test errors
     optimal_ds = []
     test_errors = []
-    runs = 2
-    max_degree = 2
+    runs = 20
+    max_degree = 7
     # iterate 20 times
     for run in range(runs):
+        train, test = train_test_split(digit_data, 0.8, split_label=False)
+        sets = n_fold(train, 5)
+        #print(test.shape)
         total_confusion = []
         # iterate through the degrees
         mean_validation_loss = np.zeros(max_degree)
@@ -331,6 +352,7 @@ def P1Q2(digit_data):
             degree = d+1
             validation_error = 0
             for (training, validation) in sets:
+                #print(training.shape)
                 # split the data into features and labeels 
                 x_train = np.array(training)[:,1:]
                 y_train = np.array(training)[:,0]
@@ -358,9 +380,11 @@ def P1Q2(digit_data):
         # fitting
         _, _ = model.fit()
         # getting the test loss
+        print("test after validation")
         test_loss, confusion_mtr = model.test(x_test, y_test, detail=True)
         test_errors.append(test_loss)
         # getting the list of confusion matrix 
+        print(confusion_mtr)
         total_confusion.append(confusion_mtr)
     
     # calculate mean d and test error
@@ -382,9 +406,257 @@ def P1Q2(digit_data):
     print("d: ", d_mean, " +- ", d_std)
     return np.mean(total_confusion, axis=0), np.std(total_confusion, axis=0)
 
+def P1Q4(digit_data):
+    """
+    Question 4
+    """
+    iterations = 40
+    all_errors = np.zeros(digit_data.shape[0])
+    for i in range(iterations):
+        optimal_degree = 2
+        # split the data randomly into train and test portions
+        x_train, y_train, x_test, y_test = train_test_split(digit_data, 0.8)
+        # define the multiclass classifier
+        model = onevrest(x_train, y_train, degree=optimal_degree)
+        # fitting, also gets the train loss
+        _, _ = model.fit()
 
+        x_full = digit_data[:,1:]
+        y_full = digit_data[:,0]
+        # testing will the whole dataset
+        _, each_error = model.test(x_full, y_full, count_each=True)
+        all_errors += each_error
+        print("iteration: ", i)
 
+    worst_five_index = np.argsort(all_errors)[-5:]
+    worst_five = []
+    for i in range(worst_five_index.size):
+        worst_five.append(digit_data[worst_five_index[i]])
+    
+    # show the digits
+    visualize(np.array(worst_five))
+
+def P1Q51(digit_data):
+    all_train_loss = []
+    all_test_loss = []
+    runs = 20
+    c = np.arange(1,8)/1000
+    # iterate through the degrees
+    for d in range(0,7):
+        degree = c[d]
+        train_list = []
+        test_list = []
+        # for each degree, iterate
+        for run in range(runs):
+            # split the data randomly into train and test portions
+            x_train, y_train, x_test, y_test = train_test_split(digit_data, 0.8)
+            # define the multiclass classifier
+            model = onevrest(x_train, y_train, degree=degree, kernel_function=gaussian_kernel)
+            # fitting, also gets the train loss
+            weights, loss = model.fit()
+            # getting the test loss
+            test_loss = model.test(x_test, y_test)
+
+            print("degree: ", degree, " iter: ", run, " train loss: ", loss)
+            print("test loss: ", test_loss)
+            train_list.append(loss)
+            test_list.append(test_loss)
+        # calculate the mean and standard deviation of train and test errors
+        train_mean, train_std = np.mean(train_list), np.std(train_list)
+        test_mean, test_std = np.mean(test_list), np.std(test_list)
+        all_train_loss.append((train_mean, train_std))
+        all_test_loss.append((test_mean, test_std))
+
+    print(all_train_loss, all_test_loss)
+
+def P1Q52(digit_data):
+    """
+    Question 5 repeating q2
+    """
+    # initialize list of d and test errors
+    optimal_ds = []
+    test_errors = []
+    runs = 20
+    max_degree = 7
+    # iterate 20 times
+    for run in range(runs):
+        train, test = train_test_split(digit_data, 0.8, split_label=False)
+        sets = n_fold(train, 5)
+        #print(test.shape)
+        total_confusion = []
+        # iterate through the degrees
+        mean_validation_loss = np.zeros(max_degree)
+        c = np.arange(1,8)/1000
+        for d in range(max_degree):
+            print("run ", run, "degree ", d+1)
+            degree = c[d]
+            validation_error = 0
+            for (training, validation) in sets:
+                #print(training.shape)
+                # split the data into features and labeels 
+                x_train = np.array(training)[:,1:]
+                y_train = np.array(training)[:,0]
+                x_validate = np.array(validation)[:,1:]
+                y_validate = np.array(validation)[:,0]
+                # define the multiclass classifier
+                model = onevrest(x_train, y_train, degree=degree, kernel_function=gaussian_kernel)
+                # fitting
+                _, _ = model.fit()
+                # getting the test loss
+                validation_loss = model.test(x_validate, y_validate)
+                validation_error += validation_loss
+
+            mean_validation_loss[d] = validation_error
+        # add the optimal d value to the list
+        optimal_d = np.argmin(mean_validation_loss) + 1
+        optimal_ds.append(optimal_d)
+
+        # training using the optimal d
+        x_full_train = np.array(train)[:,1:]
+        y_full_train = np.array(train)[:,0]
+        x_test = np.array(test)[:,1:]
+        y_test = np.array(test)[:,0]
+        model = onevrest(x_full_train, y_full_train, degree=optimal_d, kernel_function=gaussian_kernel)
+        # fitting
+        _, _ = model.fit()
+        # getting the test loss
+        print("test after validation")
+        test_loss, confusion_mtr = model.test(x_test, y_test, detail=True)
+        test_errors.append(test_loss)
+        # getting the list of confusion matrix 
+        print(confusion_mtr)
+        total_confusion.append(confusion_mtr)
+    
+    # calculate mean d and test error
+    test_mean = np.mean(test_errors)
+    test_std = np.std(test_errors)
+    d_mean = np.mean(optimal_ds)
+    d_std = np.std(optimal_ds)
+
+    # calculate the mean and std of confusion matrix elements
+    print(np.mean(total_confusion, axis=0), np.std(total_confusion, axis=0))
+    # confusion_mean = np.mean(total_confusion, axis=0).tolist()
+    # confusion_std = np.std(total_confusion, axis=0).tolist()
+    # overall_confusion_mtr = np.zeros((10,10)).tolist()
+    # for i in range(10):
+    #     for j in range(10):
+    #         overall_confusion_mtr[i][j] = (confusion_mean[i][j], confusion_std[i][j])
+    # print(overall_confusion_mtr)
+    print("test: ", test_mean, " +- ", test_std)
+    print("d: ", d_mean, " +- ", d_std)
+    return np.mean(total_confusion, axis=0), np.std(total_confusion, axis=0)
+
+def P1Q61(digit_data):
+    """
+    Part 1
+    """
+    all_train_loss = []
+    all_test_loss = []
+    runs = 20
+    # iterate through the degrees
+    for d in range(0,7):
+        degree = d+1
+        train_list = []
+        test_list = []
+        # for each degree, iterate
+        for run in range(runs):
+            # split the data randomly into train and test portions
+            x_train, y_train, x_test, y_test = train_test_split(digit_data, 0.8)
+            # define the multiclass classifier
+            model = onevone(x_train, y_train, degree=degree)
+            # fitting, also gets the train loss
+            weights, loss = model.fit()
+            # getting the test loss
+            test_loss = model.test(x_test, y_test)
+
+            print("degree: ", degree, " iter: ", run, " train loss: ", loss)
+            print("test loss: ", test_loss)
+            train_list.append(loss)
+            test_list.append(test_loss)
+        # calculate the mean and standard deviation of train and test errors
+        train_mean, train_std = np.mean(train_list), np.std(train_list)
+        test_mean, test_std = np.mean(test_list), np.std(test_list)
+        all_train_loss.append((train_mean, train_std))
+        all_test_loss.append((test_mean, test_std))
+
+    print(all_train_loss, all_test_loss)
+
+def P1Q62(digit_data):
+    """
+    Question 2
+    """
+    # initialize list of d and test errors
+    optimal_ds = []
+    test_errors = []
+    runs = 20
+    max_degree = 7
+    # iterate 20 times
+    for run in range(runs):
+        train, test = train_test_split(digit_data, 0.8, split_label=False)
+        sets = n_fold(train, 5)
+        #print(test.shape)
+        total_confusion = []
+        # iterate through the degrees
+        mean_validation_loss = np.zeros(max_degree)
+        for d in range(max_degree):
+            print("run ", run, "degree ", d+1)
+            degree = d+1
+            validation_error = 0
+            for (training, validation) in sets:
+                #print(training.shape)
+                # split the data into features and labeels 
+                x_train = np.array(training)[:,1:]
+                y_train = np.array(training)[:,0]
+                x_validate = np.array(validation)[:,1:]
+                y_validate = np.array(validation)[:,0]
+                # define the multiclass classifier
+                model = onevone(x_train, y_train, degree=degree)
+                # fitting
+                _, _ = model.fit()
+                # getting the test loss
+                validation_loss = model.test(x_validate, y_validate)
+                validation_error += validation_loss
+
+            mean_validation_loss[d] = validation_error
+        # add the optimal d value to the list
+        optimal_d = np.argmin(mean_validation_loss) + 1
+        optimal_ds.append(optimal_d)
+
+        # training using the optimal d
+        x_full_train = np.array(train)[:,1:]
+        y_full_train = np.array(train)[:,0]
+        x_test = np.array(test)[:,1:]
+        y_test = np.array(test)[:,0]
+        model = onevone(x_full_train, y_full_train, degree=optimal_d)
+        # fitting
+        _, _ = model.fit()
+        # getting the test loss
+        print("test after validation")
+        test_loss, confusion_mtr = model.test(x_test, y_test, detail=True)
+        test_errors.append(test_loss)
+        # getting the list of confusion matrix 
+        print(confusion_mtr)
+        total_confusion.append(confusion_mtr)
+    
+    # calculate mean d and test error
+    test_mean = np.mean(test_errors)
+    test_std = np.std(test_errors)
+    d_mean = np.mean(optimal_ds)
+    d_std = np.std(optimal_ds)
+
+    # calculate the mean and std of confusion matrix elements
+    print(np.mean(total_confusion, axis=0), np.std(total_confusion, axis=0))
+    # confusion_mean = np.mean(total_confusion, axis=0).tolist()
+    # confusion_std = np.std(total_confusion, axis=0).tolist()
+    # overall_confusion_mtr = np.zeros((10,10)).tolist()
+    # for i in range(10):
+    #     for j in range(10):
+    #         overall_confusion_mtr[i][j] = (confusion_mean[i][j], confusion_std[i][j])
+    # print(overall_confusion_mtr)
+    print("test: ", test_mean, " +- ", test_std)
+    print("d: ", d_mean, " +- ", d_std)
+    return np.mean(total_confusion, axis=0), np.std(total_confusion, axis=0)
 
 if __name__ == "__main__":
     data = np.loadtxt('Data\zipcombo.dat')
-    P1Q2(data)
+    P1Q4(data)
